5fa0b08808871de64f349100b4ba800c622521ff
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-06-04 19:04:05 +0200

    added firebase database

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log(isLoggedIn);
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  useEffect(() => {
+    fetch(
+      "https://redux-http-86b68-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   return (
     <div className="App">
       {!isLoggedIn && <Auth />}
